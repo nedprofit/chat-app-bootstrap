a3d88b4aa3db5d8771ca565752ce89308bb3768a
Паращевин Григорий Андреевич <gparaschevin@stsoft.ru>
2024-01-01 20:26:24 +0300

    fix factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@example.com" }
+    sequence(:email) { |n| "user#{n}@gmail.com" }
     password { "password" }
   end
 end
